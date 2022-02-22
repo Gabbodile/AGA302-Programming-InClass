43698d90683b2afae11fd41e1af263ee16cbe323
Gabbodile <94325069+Gabbodile@users.noreply.github.com>
2022-02-22 19:58:55 +1100

    base scripts

---
+++ Assets/Scripts/FiringPoint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringPoint : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 1000f;
+    public Transform firingPoint;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject projectileInstance;
+            projectileInstance = Instantiate(projectilePrefab, firingPoint.posistion, firingPoint.rotation);
+            projectileInstance.GetComponent<Rigidbody>().AddForce(firingPoint.forward * projectileSpeed);
+        }
+    }
+}
